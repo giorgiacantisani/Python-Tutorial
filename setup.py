ed6d6d8df064dbf04fcdc35e33c815ecf564dca9
giorgiacantisani <giorgia.cantisani@gmail.com>
2019-05-02 12:03:46 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='MAD-EEG: an EEG dataset for decoding auditory attention to a target instrument in polyphonic music',
+    author='Cantisani Giorgia',
+    license='MIT',
+)
